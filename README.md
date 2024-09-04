74875f2347ab5626a76916f797ca3f4773ef1068
Ian Ruchlin <ianruchlin@gmail.com>
2024-09-04 08:21:48 -0400

    Dev (#1)
    * Shuffle prefix_list
    
    * Clean up
    
    * Result for c16

---
+++ README.md
@@ -0,0 +1,5 @@
+# The number of loops of length 64 on a square grid that turn at each step and return to start in the original direction without overlapping is 37807494490.
+
+Rotations and reflections of a given loop are not counted.
+
+[OEIS A000644](https://oeis.org/A000644)
--- tangle/count_mpi.py
+++ tangle/count_mpi.py
@@ -43,6 +43,10 @@ if rank == root:
         (rem_list_root[:, 0] >= idx_min_half) &
         (rem_list_root[:, 0] <= idx_max_half)
     ]
+
+    rng = np.random.default_rng(1234)
+    rng.shuffle(prefix_list_root)
+
     len_prefix_list = len(prefix_list_root)
 else:
     rem_list_root = None
@@ -53,20 +57,29 @@ else:
 len_rem_list = comm.bcast(len_rem_list, root=root)
 len_prefix_list = comm.bcast(len_prefix_list, root=root)
 
-index_list = np.zeros((size, 2), dtype=np.int_)
-index_list[0, 1] = len_prefix_list // size + 1
-for r in range(1, size):
-    index_list[r, 0] = index_list[r - 1, 1]
-    index_list[r, 1] = index_list[r, 0] + len_prefix_list // size + (1 if r < len_prefix_list % size else 0)
-
-win_rem_list = MPI.Win.Allocate_shared(2 * len_rem_list * MPI.UINT64_T.Get_size(), MPI.UINT64_T.Get_size(), comm=comm)
-win_prefix_list = MPI.Win.Allocate_shared(2 * len_prefix_list * MPI.UINT64_T.Get_size(), MPI.UINT64_T.Get_size(), comm=comm)
-
+win_rem_list = MPI.Win.Allocate_shared(
+    2 * len_rem_list * MPI.UINT64_T.Get_size(),
+    MPI.UINT64_T.Get_size(),
+    comm=comm,
+)
 rem_buf, _ = win_rem_list.Shared_query(root)
-rem_list = np.ndarray(buffer=rem_buf, dtype=np.uint64, shape=(len_rem_list, 2))
+rem_list = np.ndarray(
+    buffer=rem_buf,
+    dtype=np.uint64,
+    shape=(len_rem_list, 2),
+)
 
+win_prefix_list = MPI.Win.Allocate_shared(
+    2 * len_prefix_list * MPI.UINT64_T.Get_size(),
+    MPI.UINT64_T.Get_size(),
+    comm=comm,
+)
 prefix_buf, _ = win_prefix_list.Shared_query(root)
-prefix_list = np.ndarray(buffer=prefix_buf, dtype=np.uint64, shape=(len_prefix_list, 2))
+prefix_list = np.ndarray(
+    buffer=prefix_buf,
+    dtype=np.uint64,
+    shape=(len_prefix_list, 2),
+)
 
 if rank == root:
     np.copyto(rem_list, rem_list_root)
@@ -74,9 +87,21 @@ if rank == root:
     np.copyto(prefix_list, prefix_list_root)
     prefix_list_root = None
 
+index_list = np.zeros((size, 2), dtype=np.int_)
+index_list[0, 1] = len_prefix_list // size + 1
+for r in range(1, size):
+    index_list[r, 0] = index_list[r - 1, 1]
+    offset = 1 if r < len_prefix_list % size else 0
+    index_list[r, 1] = index_list[r, 0] + len_prefix_list // size + offset
+
+index_lower = index_list[rank, 0]
+index_upper = index_list[rank, 1]
+
 comm.Barrier()
 
-prefix_list[index_list[rank, 0]:index_list[rank, 1]] = prefix_list[index_list[rank, 0]:index_list[rank, 1]][np.argsort(prefix_list[index_list[rank, 0]:index_list[rank, 1], 1], kind='stable')]
+prefix_list[index_lower:index_upper] = prefix_list[index_lower:index_upper][
+        np.argsort(prefix_list[index_lower:index_upper, 1], kind="stable")
+]
 
 comm.Barrier()
 
@@ -84,8 +109,14 @@ if rank == root:
     print(f"precompute time {time.time() - start_time}")
 
 start_time = time.time()
-t_count = count_tangles(n, rem_list, prefix_list[index_list[rank, 0]:index_list[rank, 1]])
-print(f"rank {rank}, index [{index_list[rank, 0]}, {index_list[rank, 1]}], tangles {t_count}, time {time.time() - start_time}")
+
+t_count = count_tangles(n, rem_list, prefix_list[index_lower:index_upper])
+print(
+    f"rank {rank}, "
+    f"index [{index_lower}, {index_upper}], "
+    f"tangles {t_count}, "
+    f"time {time.time() - start_time}"
+)
 total_t_count = comm.reduce(t_count, op=MPI.SUM, root=root)
 
 if rank == root:
